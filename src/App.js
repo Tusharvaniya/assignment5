52518fee9b9a59fae97fa2f806936c593b715ea0
tushar vaniya <tushar.vaniya.hs@gmail.com>
2021-02-25 16:55:42 +0530

    assignment 5 use of hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Fragment, useState, useEffect } from "react";
 
 function App() {
+  let [char_counter, changeCharCount] = useState(0);
+  let [word_counter, changeWordCount] = useState(0);
+  let [lastWord, ab] = useState(null);
+  let [history, setHistory] = useState([]);
+
+  const i_will_count = (event) => {
+    const text = event.target.value;
+    const characters = text.replaceAll(" ", "").length;
+    changeCharCount((char_counter = characters));
+
+    let words = null;
+    if (characters > 0) {
+      words = text.trim().split(/\s+/);
+      changeWordCount((word_counter = words.length));
+      ab((lastWord = words[words.length - 2]));
+    }
+  };
+
+  useEffect(() => {
+    if (lastWord) {
+      let newarr = [...history];
+      newarr.push(lastWord);
+      setHistory((history = [...newarr]));
+    }
+    ab((lastWord = null));
+  }, [word_counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <header className="App-header">Character and Word counter</header>
+      </div>
+      <div className="Container">
+        <div>
+          <textarea
+            className="textbox"
+            name="text1"
+            id="text1"
+            onChange={(event) => i_will_count(event)}
+          />
+        </div>
+        <div className="charCounter">
+          Total Characters : <strong>{char_counter}</strong>
+        </div>
+        <div className="wordCounter">
+          Total Words : <strong>{word_counter}</strong>
+        </div>
+        <div>Your word history below :</div>
+        <div>
+          <ul className="historyWords">
+            {history.map((h) => {
+              return <li>{h}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
